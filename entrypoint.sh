8268ada7b4308a324b10f48cdd5dc257a17041d3
Vitor Gomes <vitor.gomes@warrenbrasil.com.br>
2020-06-25 11:49:08 -0300

    removes wrong parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,6 @@ EOF
 # All other flags are optional via the `args:` directive.
 sh -c "aws s3 rm ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-rm-action \
-              --no-progress \
               ${ENDPOINT_APPEND} $*"
 
 # Clear out credentials after we're done.
